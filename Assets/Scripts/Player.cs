9eb9109ef5566d3d6c3faf694ec905dd061b1fc5
JoonaMartinkauppi <61461699+JoonaMartinkauppi@users.noreply.github.com>
2021-04-06 14:47:40 +0300

    testi
    testi

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // ========= MOVEMENT =================
+    public float speed = 4;
+
+    // =========== MOVEMENT ==============
+    Rigidbody2D rigidbody2d;
+    Vector2 currentInput;
+    Vector2 lookDirection = new Vector2(1, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // =========== MOVEMENT ==============
+        rigidbody2d = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // ============== MOVEMENT ======================
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+        currentInput = move;
+
+        // ======== INTERACT ==========
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, lookDirection, 1.5f, 1 << LayerMask.NameToLayer("box"));
+            if (hit.collider != null)
+            {
+                Debug.Log("Box hit" + hit.collider.gameObject);
+                GameController.instance.LoadPlatformer();
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+
+        position = position + currentInput * speed * Time.deltaTime;
+
+        rigidbody2d.MovePosition(position);
+    }
+}
