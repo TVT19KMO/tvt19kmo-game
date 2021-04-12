e80b5faf2fa9bb1e3a8c8e28db21282218532abd
JoonaMartinkauppi <61461699+JoonaMartinkauppi@users.noreply.github.com>
2021-04-12 06:55:00 +0300

    Demo / tutorial -kentän pohjaa
    TODO: taustamusiikki pauselle kun pelitauolla, coin/trap scriptien liittäminen tilemappiin. Grafiikat placeholdereita.

--- Assets/Scripts/PlayerMovementScript.cs
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class PlayerMovementScript : MonoBehaviour
 {
     public float speed;
+
     public VariableJoystick variableJoystick;
     public Rigidbody2D rb;
     public float JumpForce;
@@ -25,16 +26,18 @@ public class PlayerMovementScript : MonoBehaviour
     public void FixedUpdate()
     {
       //  Vector2 dire = variableJoystick.Vertical * Vector2.up + variableJoystick.Horizontal * Vector2.right;
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.5f, collisionlayer);
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.6f, collisionlayer);
         if(isGrounded)
         {
-            rb.AddForce(Vector2.right* variableJoystick.Horizontal * MovementSpeed, ForceMode2D.Impulse);
-            rb.velocity = Vector2.ClampMagnitude(rb.velocity, MaxSpeed);
+            rb.velocity = new Vector2(variableJoystick.Horizontal * speed, rb.velocity.y);
+ //           rb.AddForce(Vector2.right* variableJoystick.Horizontal * MovementSpeed, ForceMode2D.Impulse);
+ //           rb.velocity = Vector2.ClampMagnitude(rb.velocity, MaxSpeed);
         }
         else
         {
-            rb.AddForce(Vector2.right* variableJoystick.Horizontal * AirSpeed, ForceMode2D.Impulse);
+//            rb.AddForce(Vector2.right* variableJoystick.Horizontal * AirSpeed, ForceMode2D.Impulse);
             rb.AddForce(Vector2.up* variableJoystick.Vertical * AirSpeed, ForceMode2D.Impulse);
+            rb.velocity = new Vector2(variableJoystick.Horizontal * speed, rb.velocity.y);
             rb.velocity = Vector2.ClampMagnitude(rb.velocity, MaxSpeed);
         }
     }
