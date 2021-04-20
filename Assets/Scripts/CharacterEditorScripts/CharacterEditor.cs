6817b5f4fe9bd17a6b0dadcc97b43b35ad9a04f5
Arttu Rusanen <54593874+ArtRus97@users.noreply.github.com>
2021-04-20 16:22:21 +0300

    Added changes from Joystick branch

--- Assets/Scripts/CharacterEditorScripts/CharacterEditor.cs
+++ Assets/Scripts/CharacterEditorScripts/CharacterEditor.cs
@@ -7,6 +7,8 @@ using UnityEngine.UI;
 
 public class CharacterEditor : MonoBehaviour
 {
+    public static CharacterEditor instance;
+
     public GameObject panel;
     public GameObject character;
 
@@ -18,8 +20,47 @@ public class CharacterEditor : MonoBehaviour
     public Color color3;
     public Color color4;
     public Color color5;
-
     public int whatColor;
+
+    public GameObject LSneaker;
+    public GameObject RSneaker;
+    public SpriteRenderer left_sneaker;
+    public SpriteRenderer right_sneaker;
+    public Sprite[] spriteArray_left;
+    public Sprite[] spriteArray_right;
+
+    public int ChosenSneaker;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Start()
+    {
+        string headColor = "#" + PlayerPrefs.GetString("headColor", null);
+        Color headclr;
+        if (ColorUtility.TryParseHtmlString(headColor, out headclr))
+        {
+            head.color = headclr;
+        }
+
+        ChosenSneaker = PlayerPrefs.GetInt("SneakersColor", 1);
+        if (ChosenSneaker != 1)
+        {
+            LSneaker.SetActive(true);
+            RSneaker.SetActive(true);
+            left_sneaker.sprite = spriteArray_left[ChosenSneaker - 1];
+            right_sneaker.sprite = spriteArray_right[ChosenSneaker - 1];
+        }
+    }
 
     public void ChangePanelState(bool state)
     {
@@ -28,7 +69,9 @@ public class CharacterEditor : MonoBehaviour
 
     public void SaveCharacter()
     {
-        PrefabUtility.SaveAsPrefabAsset(character, "Assets/Prefabs/Player.prefab");
+        PlayerPrefs.SetString("headColor", ColorUtility.ToHtmlStringRGBA(head.color));
+        PlayerPrefs.SetInt("SneakersColor", ChosenSneaker);
+        Debug.Log(ColorUtility.ToHtmlStringRGBA(head.color));
     }
 
     public void ChangeHeadColor(int color)
@@ -57,5 +100,48 @@ public class CharacterEditor : MonoBehaviour
         }
 
         squareHeadDisplay.color = head.color;
+    }
+
+    public void NextSneakers()
+    {
+        int SneakerCount = spriteArray_left.Length;
+        if (ChosenSneaker < SneakerCount)
+        {
+            ChosenSneaker++;
+            LSneaker.SetActive(true);
+            RSneaker.SetActive(true);
+        }
+        else
+        {
+            ChosenSneaker = 1;
+            LSneaker.SetActive(false);
+            RSneaker.SetActive(false);
+        }
+        left_sneaker.sprite = spriteArray_left[ChosenSneaker - 1];
+        right_sneaker.sprite = spriteArray_right[ChosenSneaker - 1];
+        Debug.Log(ChosenSneaker);
+    }
+
+    public void PrevSneakers()
+    {
+        int SneakerCount = spriteArray_left.Length;
+        if (ChosenSneaker > 1)
+        {
+            ChosenSneaker--;
+            if (ChosenSneaker == 1)
+            {
+                LSneaker.SetActive(false);
+                RSneaker.SetActive(false);
+            }
+        }
+        else
+        {
+            ChosenSneaker = SneakerCount;
+            LSneaker.SetActive(true);
+            RSneaker.SetActive(true);
+        }
+        left_sneaker.sprite = spriteArray_left[ChosenSneaker - 1];
+        right_sneaker.sprite = spriteArray_right[ChosenSneaker - 1];
+        Debug.Log(ChosenSneaker);
     }
 }
