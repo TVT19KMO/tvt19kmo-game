298a77b3518b824b36a632d561c9434496ff8669
Arttu Rusanen <54593874+ArtRus97@users.noreply.github.com>
2021-04-22 11:35:41 +0300

    Added updates from Joytick branch and updated sprites

--- Assets/Data.cs
+++ Assets/Data.cs
@@ -6,7 +6,12 @@ public static class Data
 {
     public static Sprite[] LeftSneakers;
     public static Sprite[] RightSneakers;
-    public static Sprite[] Caps;
-    public static Sprite[] Beanies;
+    public static Sprite[] Hats;
+    public static Sprite[] LeftLegs;
+    public static Sprite[] RightLegs;
+    public static Sprite[] Bottoms;
+    public static Sprite[] Tops;
+    public static Sprite[] LeftArms;
+    public static Sprite[] RightArms;
     public static string[] HeadColors;
 }
--- Assets/Scripts/CharacterEditorScripts/CharacterEditor.cs
+++ Assets/Scripts/CharacterEditorScripts/CharacterEditor.cs
@@ -24,11 +24,27 @@ public class CharacterEditor : MonoBehaviour
     public Sprite[] spriteArray_right;
     public int ChosenSneaker;
 
-    public GameObject cap;
+    public GameObject hairSlot;
     public SpriteRenderer caprenderer;
-    public Sprite[] caps;
-    public int ChosenCap;
+    public Sprite[] hats;
+    public int ChosenHat;
 
+    public SpriteRenderer LeftArm;
+    public SpriteRenderer RightArm;
+    public SpriteRenderer Top;
+    public Sprite[] larms;
+    public Sprite[] rarms;
+    public Sprite[] tops;
+    public int ChosenJacket;
+
+    public SpriteRenderer LeftLeg;
+    public SpriteRenderer RightLeg;
+    public SpriteRenderer Bottom;
+    public Sprite[] LeftLegs;
+    public Sprite[] RightLegs;
+    public Sprite[] Bottoms;
+    public int ChosenPants;
+    
     void Awake()
     {
         if (instance == null)
@@ -62,19 +78,43 @@ public class CharacterEditor : MonoBehaviour
         Data.LeftSneakers = spriteArray_left;
         Data.RightSneakers = spriteArray_right;
 
-        ChosenCap = PlayerPrefs.GetInt("Cap", 0);
-        if (ChosenCap != 0)
+        ChosenHat = PlayerPrefs.GetInt("Hat", 0);
+        if (ChosenHat != 0)
         {
-            caprenderer.sprite = caps[ChosenCap];
+            caprenderer.sprite = hats[ChosenHat];
         }
-        Data.Caps = caps;
+        Data.Hats = hats;
+
+        ChosenJacket = PlayerPrefs.GetInt("Jacket", 0);
+        if (ChosenJacket != 0)
+        {
+            LeftArm.sprite = larms[ChosenJacket];
+            RightArm.sprite = rarms[ChosenJacket];
+            Top.sprite = tops[ChosenJacket];
+        }
+        Data.Tops = tops;
+        Data.LeftArms = larms;
+        Data.RightArms = rarms;
+
+        ChosenPants = PlayerPrefs.GetInt("Pants", 0);
+        if (ChosenPants != 0)
+        {
+            LeftLeg.sprite = LeftLegs[ChosenPants];
+            RightLeg.sprite = RightLegs[ChosenPants];
+            Bottom.sprite = Bottoms[ChosenPants];
+        }
+        Data.Bottoms = Bottoms;
+        Data.LeftLegs = LeftLegs;
+        Data.RightLegs = RightLegs;
     }
 
     public void SaveCharacter()
     {
         PlayerPrefs.SetInt("HeadColor", ChosenHeadColor);
         PlayerPrefs.SetInt("SneakersColor", ChosenSneaker);
-        PlayerPrefs.SetInt("Cap", ChosenCap);
+        PlayerPrefs.SetInt("Hat", ChosenHat);
+        PlayerPrefs.SetInt("Jacket", ChosenJacket);
+        PlayerPrefs.SetInt("Pants", ChosenPants);
     }
 
     public void prevHeadColor()
@@ -146,31 +186,91 @@ public class CharacterEditor : MonoBehaviour
         Debug.Log(ChosenSneaker);
     }
 
-    public void nextCap()
+    public void nextHat()
     {
-        int capCount = caps.Length;
-        if (ChosenCap < capCount - 1)
+        int hatCount = hats.Length;
+        if (ChosenHat < hatCount - 1)
         {
-            ChosenCap++;
+            ChosenHat++;
         }
         else
         {
-            ChosenCap = 0;
+            ChosenHat = 0;
         }
-        caprenderer.sprite = caps[ChosenCap];
+        caprenderer.sprite = hats[ChosenHat];
     }
 
-    public void prevCap()
+    public void prevHat()
     {
-        int capCount = caps.Length;
-        if (ChosenCap > 0)
+        int capCount = hats.Length;
+        if (ChosenHat > 0)
         {
-            ChosenCap--;
+            ChosenHat--;
         }
         else
         {
-            ChosenCap = capCount - 1;
+            ChosenHat = capCount - 1;
         }
-        caprenderer.sprite = caps[ChosenCap];
+        caprenderer.sprite = hats[ChosenHat];
+    }
+
+    public void nextJacket()
+    {
+        if (ChosenJacket < tops.Length - 1)
+        {
+            ChosenJacket++;
+        }
+        else
+        {
+            ChosenJacket = 0;
+        }
+        LeftArm.sprite = larms[ChosenJacket];
+        RightArm.sprite = rarms[ChosenJacket];
+        Top.sprite = tops[ChosenJacket];
+    }
+
+    public void prevJacket()
+    {
+        if (ChosenJacket > 0)
+        {
+            ChosenJacket--;
+        }
+        else
+        {
+            ChosenJacket = tops.Length - 1;
+        }
+        LeftArm.sprite = larms[ChosenJacket];
+        RightArm.sprite = rarms[ChosenJacket];
+        Top.sprite = tops[ChosenJacket];
+    }
+
+    public void nextPants()
+    {
+        if (ChosenPants < Bottoms.Length - 1)
+        {
+            ChosenPants++;
+        }
+        else
+        {
+            ChosenPants = 0;
+        }
+        LeftLeg.sprite = LeftLegs[ChosenPants];
+        RightLeg.sprite = RightLegs[ChosenPants];
+        Bottom.sprite = Bottoms[ChosenPants];
+    }
+
+    public void prevPants()
+    {
+        if (ChosenPants > 0)
+        {
+            ChosenPants--;
+        }
+        else
+        {
+            ChosenPants = Bottoms.Length - 1;
+        }
+        LeftLeg.sprite = LeftLegs[ChosenPants];
+        RightLeg.sprite = RightLegs[ChosenPants];
+        Bottom.sprite = Bottoms[ChosenPants];
     }
 }
