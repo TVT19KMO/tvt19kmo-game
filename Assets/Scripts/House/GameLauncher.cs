9ea0e5a973fcd4d306e535a54d15f4635a5ae296
Arttu Rusanen <54593874+ArtRus97@users.noreply.github.com>
2021-04-22 09:53:01 +0300

    Added bedroom and other updates to house

--- Assets/Scripts/House/GameLauncher.cs
+++ Assets/Scripts/House/GameLauncher.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TV : MonoBehaviour
+public class GameLauncher : MonoBehaviour
 {
     public GameObject GameMenu;
 
